02ed0dd48e15c2a73a492e298bd0509f8c6959f9
David Gustavsson <david.e.gustavsson@gmail.com>
2020-08-18 22:14:15 +0200

    [WIP] add spectrum approximation for black body radiators

--- src/UnitfulColors.jl
+++ src/UnitfulColors.jl
@@ -1,9 +1,12 @@
 module UnitfulColors
 using Unitful
 import Colors.colormatch
+import Colors.XYZ
+import Colors.xyY
 import PhysicalConstants.CODATA2018.h
 import PhysicalConstants.CODATA2018.c_0
 import PhysicalConstants.CODATA2018.b
+import PhysicalConstants.CODATA2018.k_B
 
 export colormatch
 
@@ -40,13 +43,28 @@ A color approximately matching the peak wavelength of a black-body radiator with
 temperature `T`.
 
 If given a second argument `:spectrum`, tries to approximate the actual color spectrum
-of such a black-body radiator (not implemented)
+of such a black-body radiator.
 """
 function colormatch(T::Unitful.Temperature, interp::Symbol=:peak)
-    interp==:peak && return colormatch(b/(T+0u"K"))
-    interp==:spectrum && begin
-        print("Spectrum not yet implemented")
-        return colormatch(T,:peak)
+    T = T + 0.0u"K"
+    interp==:peak && return colormatch(b/T)
+    if interp==:spectrum
+        # Cubic spline from US patent US2003095138 (A1)
+        T<=1667.0u"K" && return XYZ{Float64}(0,0,0.5)
+        T>=25000.0u"K" && return XYZ{Float64}(0,0,0.5)
+        x_c = (T <= 4000.0u"K"
+               ? -0.2661239*(1e3u"K"/T)^3 - 0.2343589*(1e3u"K"/T)^2 + 0.8776956*(1e3u"K"/T) + 0.179910
+               : -3.0258469*(1e3u"K"/T)^3 + 2.1070379*(1e3u"K"/T)^2 + 0.2226347*(1e3u"K"/T) + 0.240390
+              )
+        y_c = (T <= 2222.0u"K"
+               ? -1.1063814x_c^3 - 1.34811020x_c^2 + 2.18555832x_c - 0.20219683
+               : (T <= 4000.0u"K"
+                  ? -0.9549476x_c^3 - 1.37418593x_c^2 + 2.09137015x_c - 0.16748867
+                  : 3.0817580x_c^3 - 5.87338670x_c^2 + 3.75112997x_c - 0.37001483
+                 )
+              )
+        Y = 1
+        return xyY{Float64}(x_c,y_c,Y)
     end
 end
 
