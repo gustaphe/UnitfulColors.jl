6abe287c1c07a4ebf3081c056c920d961eb02a68
David Gustavsson <david.e.gustavsson@gmail.com>
2020-12-09 15:11:14 +0100

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
+import Colors: XYZ, xyY
+import Unitful.@u_str
 using UnitfulColors
 using Test
 
 @testset "UnitfulColors.jl" begin
-    # Write your tests here.
+    @test colormatch(612.2u"nm") == colormatch(612.2)
+    @test typeof(colormatch(600u"THz")) <:  XYZ
+    @test typeof(colormatch(2.3u"eV"))  <:  XYZ
+    @test typeof(colormatch(5000u"K"))  <:  XYZ
+    @test typeof(colormatch(1.5e4u"K",:spectrum)) <:  xyY
 end
