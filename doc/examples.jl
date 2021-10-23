8d7b480c435314e3197baf84aa7f34f4e62e7d29
David Gustavsson <david.e.gustavsson@gmail.com>
2021-10-23 12:12:28 +0200

    More robust unit handling, formatting, improve examples

--- doc/examples.jl
+++ doc/examples.jl
@@ -1,26 +1,31 @@
 using UnitfulColors
-using Plots,Unitful,Colors,LaTeXStrings
+using Plots, Unitful, UnitfulRecipes, Colors, Latexify, UnitfulLatexify
 
-function showUnitColor(title,x,varargs...)
-    heatmap(
-        ustrip(x), [-1,1], repeat(ustrip(x),2,1),
+function showUnitColor(title, x, varargs...)
+    return heatmap(
+        x,
+        [-1, 1],
+        repeat(x, 2, 1);
         background=:black,
-        color=colormatch.(x,varargs...),
+        color=colormatch.(x, varargs...),
         legend=false,
         colorbar=false,
-        xlabel=LaTeXString("\$\\textrm{$title } /\\mathrm{$(unit(eltype(x)))} \$"),
+        xlabel="\\textrm{$title}",
         yticks=[],
         yshowaxis=false,
-        formatter=:latex
+        fontfamily="Computer Modern",
+        unitformat=latexify,
     )
 end
 
 length = 1000;
-plot(
-     showUnitColor("Wavelength",range(350,750;length).*u"nm"),
-     showUnitColor("Frequency",range(400,800;length).*u"THz"),
-     showUnitColor("Energy",range(1.5,3.5;length).*u"eV"),
-     showUnitColor("Temperature (peak)",range(4000,8000;length).*u"K"),
-     showUnitColor("Temperature (spectrum)",range(1000,27000;length).*u"K",:spectrum),
-     layout=(5,1),
-    );
+pl = plot(
+    showUnitColor("Wavelength", range(350, 750; length) .* u"nm"),
+    showUnitColor("Frequency", range(400, 800; length) .* u"THz"),
+    showUnitColor("Energy", range(1.5, 3.5; length) .* u"eV"),
+    showUnitColor("Temperature (peak)", range(4000, 8000; length) .* u"K"),
+    showUnitColor("Temperature (spectrum)", range(1000, 27000; length) .* u"K", :spectrum);
+    layout=(5, 1),
+);
+
+savefig(pl, "examples.png")
--- src/UnitfulColors.jl
+++ src/UnitfulColors.jl
@@ -16,7 +16,7 @@ export colormatch
 A color approximately matching wavelength `λ`
 """
 function colormatch(λ::Unitful.Length)
-    return colormatch(upreferred(λ/u"nm"))
+    return colormatch(ustrip(u"nm", λ))
 end
 
 """
@@ -25,7 +25,7 @@ end
 A color approximately matching frequency `ν`
 """
 function colormatch(ν::Unitful.Frequency)
-    return colormatch(upreferred(c_0/ν))
+    return colormatch(ustrip(u"nm", c_0 / ν))
 end
 
 """
@@ -34,7 +34,7 @@ end
 A color approximately matching photon energy `E`
 """
 function colormatch(E::Unitful.Energy)
-    return colormatch(upreferred(h*c_0/E))
+    return colormatch(ustrip(u"nm", h * c_0 / E))
 end
 
 """
@@ -43,29 +43,43 @@ end
 A color approximately matching the peak wavelength of a black-body radiator with
 temperature `T`.
 
-If given a second argument `:spectrum`, tries to approximate the actual color spectrum
-of such a black-body radiator.
+If given a second argument `:spectrum`, tries to approximate the actual color spectrum of
+such a black-body radiator. If the keyword argument `Y` is given, it determines the
+brightness (default `0.75`, `0<=Y<=1`).
 """
-function colormatch(T::Unitful.Temperature, interp::Symbol=:peak)
+function colormatch(T::Unitful.Temperature, interp::Symbol=:peak; Y=0.75)
     T = T + 0.0u"K"
-    interp==:peak && return colormatch(b/T)
-    if interp==:spectrum
+    interp == :peak && return colormatch(b / T)
+    if interp == :spectrum
         # Cubic spline from US patent US2003095138 (A1)
-        T<=1667.0u"K" && return XYZ{Float64}(0,0,0)
-        T>=25000.0u"K" && return XYZ{Float64}(0,0,0)
-        x_c = (T <= 4000.0u"K"
-               ? -0.2661239*(1e3u"K"/T)^3 - 0.2343589*(1e3u"K"/T)^2 + 0.8776956*(1e3u"K"/T) + 0.179910
-               : -3.0258469*(1e3u"K"/T)^3 + 2.1070379*(1e3u"K"/T)^2 + 0.2226347*(1e3u"K"/T) + 0.240390
-              )
-        y_c = (T <= 2222.0u"K"
-               ? -1.1063814x_c^3 - 1.34811020x_c^2 + 2.18555832x_c - 0.20219683
-               : (T <= 4000.0u"K"
-                  ? -0.9549476x_c^3 - 1.37418593x_c^2 + 2.09137015x_c - 0.16748867
-                  : 3.0817580x_c^3 - 5.87338670x_c^2 + 3.75112997x_c - 0.37001483
-                 )
-              )
-        Y = 0.75
-        return xyY{Float64}(x_c,y_c,Y)
+        T <= 1667.0u"K" && return XYZ{Float64}(0, 0, 0)
+        T >= 25000.0u"K" && return XYZ{Float64}(0, 0, 0)
+        x_c = (
+            if T <= 4000.0u"K"
+                -0.2661239 * (1e3u"K" / T)^3 - 0.2343589 * (1e3u"K" / T)^2 +
+                0.8776956 * (1e3u"K" / T) +
+                0.179910
+            else
+                -3.0258469 * (1e3u"K" / T)^3 +
+                2.1070379 * (1e3u"K" / T)^2 +
+                0.2226347 * (1e3u"K" / T) +
+                0.240390
+            end
+        )
+        y_c = (
+            if T <= 2222.0u"K"
+                -1.1063814x_c^3 - 1.34811020x_c^2 + 2.18555832x_c - 0.20219683
+            else
+                (
+                if T <= 4000.0u"K"
+                    -0.9549476x_c^3 - 1.37418593x_c^2 + 2.09137015x_c - 0.16748867
+                else
+                    3.0817580x_c^3 - 5.87338670x_c^2 + 3.75112997x_c - 0.37001483
+                end
+            )
+            end
+        )
+        return xyY{Float64}(x_c, y_c, Y)
     end
 end
 
